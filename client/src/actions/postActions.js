ef0de21f7a654646d2469f3bc7db2a957e666e99
hassanyakef <yhassan@vt.edu>
2018-12-22 21:15:58 -0500

    Display posts functionality complete.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {
    ADD_POST,
-   GET_ERRORS
+   GET_ERRORS,
+   GET_POSTS,
+   POST_LOADING
 } from "./types";
 
 // Add Post
@@ -15,4 +17,26 @@ export const addPost = postData => dispatch => {
          type: GET_ERRORS,
          payload: err.response.data
       }));
+};
+
+// Get Posts
+export const getPosts = () => dispatch => {
+   dispatch(setPostLoading())
+   axios
+      .get('/api/posts')
+      .then(res => dispatch({
+         type: GET_POSTS,
+         payload: res.data
+      }))
+      .catch(err => dispatch({
+         type: GET_POSTS,
+         payload: null
+      }));
+};
+
+// Set Loading State
+export const setPostLoading = () => {
+   return {
+      type: POST_LOADING
+   }
 };
